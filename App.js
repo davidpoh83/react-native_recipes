d04f330675f91161889e8d53f86d41d08fb1b463
David Poh <david.zpoh@gmail.com>
2020-04-03 15:18:43 +0800

    Starting new recipe app

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
