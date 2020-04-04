6df21c4313fa0e97f577a7f2eba281143ed0d444
David Poh <david.zpoh@gmail.com>
2020-04-04 12:48:27 +0800

    GridTile styling and component

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 import { CATEGORIES } from '../data/dummy-data';
 
-const renderGridItem = itemData => {
-    return (
-        <View style={styles.gridItem}>
-            <Text>{itemData.item.title}</Text>
-        </View>
-    );
-};
-
 const CategoriesScreen = props => {
+    const renderGridItem = itemData => {
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onSelect={() => {
+            props.navigation.navigate('CategoryMeals', {categoryId: itemData.item.id});
+        }} />
+    };
     return (
         <FlatList
             keyExtractor={(item, index) => item.id}
@@ -22,16 +20,15 @@ const CategoriesScreen = props => {
     );
 };
 
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories'
+};
+
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150
     }
 });
 
